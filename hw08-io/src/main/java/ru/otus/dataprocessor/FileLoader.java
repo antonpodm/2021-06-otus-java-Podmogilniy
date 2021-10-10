76a884109a2039a8b3e1a13ba1ab0696fdc54bdc
Anton Podmogilniy <antonpodm@mail.ru>
2021-10-10 12:31:26 +0300

    hw08: updated solution

--- hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
@@ -1,15 +1,13 @@
 package ru.otus.dataprocessor;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import ru.otus.model.Measurement;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.util.List;
 
 public class FileLoader implements Loader {
@@ -23,41 +21,14 @@ public class FileLoader implements Loader {
     @Override
     public List<Measurement> load() {
         try {
-            var url = getClass().getClassLoader().getResource(fileName);
-            var path = url.getPath();
-            try (var bis = new BufferedInputStream(new FileInputStream(path))) {
-                var root = new ObjectMapper().readTree(bis);
-                var result = new ArrayList<Measurement>();
-                if (root.isArray()) {
-                    var array = (ArrayNode) root;
-                    array.forEach(node -> {
-                        var measurement = makeMeasurement(node);
-                        if (measurement != null) {
-                            result.add(measurement);
-                        }
-                    });
-                } else if (root.isObject()) {
-                    var measurement = makeMeasurement(root);
-                    if (measurement != null) {
-                        result.add(measurement);
-                    }
-                }
-                return result;
+            var resource = getClass().getClassLoader().getResource(fileName);
+            if (resource == null) {
+                throw new FileProcessException("file not found");
             }
-            //читает файл, парсит и возвращает результат
-        } catch (FileNotFoundException ex) {
-            throw new MyIOException("Файл не найден");
-        } catch (IOException ex) {
+            var gsonFileAsString = Files.readString(new File(resource.toURI()).toPath());
+            return new Gson().fromJson(gsonFileAsString, new TypeToken<List<Measurement>>() {}.getType());
+        } catch (IOException| URISyntaxException ex) {
             throw new MyIOException(ex.getMessage());
         }
-    }
-
-    private Measurement makeMeasurement(JsonNode node) {
-        if (node.has("name") && node.has("value")) {
-            var name = node.get("name").asText();
-            var value = node.get("value").asDouble();
-            return new Measurement(name, value);
-        }
-        return null;
     }
 }
--- hw08-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -5,19 +5,13 @@ import ru.otus.model.Measurement;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        //группирует выходящий список по name, при этом суммирует поля value
-        var result = new TreeMap<String, Double>();
-
-        data.forEach(measurement -> {
-            result.computeIfPresent(measurement.getName(), (key, val) -> val + measurement.getValue());
-            result.putIfAbsent(measurement.getName(), measurement.getValue());
-        });
-
-        return result;
+        return data.stream()
+                .collect(Collectors.groupingBy(Measurement::getName,TreeMap::new, Collectors.summingDouble(Measurement::getValue)));
     }
 }
